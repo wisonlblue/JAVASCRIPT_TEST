e3d083d108700c794f82fc2bbca6c0d14136bdbe
wilsonlblue <wilsonlblue@gmail.com>
2026-06-23 01:47:35 +0800

    202606230147

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const 文字欄=document.querySelector(".文字欄");
+const 文字欄 = document.querySelector(".文字欄");
+const 清單 = document.querySelector(".清單");
+
+function 新任務(文字){
+    if(文字欄.value === ""){
+        return;
+    }
+    const 任務 = document.createElement("li");
+    任務.textContent = 文字欄.value;
+    清單.append(任務);
+    文字欄.value = "";
+}
 
 文字欄.addEventListener("keyup",function (e) {
     if(e.key === "Enter"){
-        console.log(文字欄.value);
-
+        新任務();
     }
 }); 
-
